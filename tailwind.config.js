fa45e9002e9c76aafa6bd684d4bdcc15845c7593
Sandree <78666373+sandro21-glitch@users.noreply.github.com>
2023-02-03 19:56:56 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        // ### Primary
+        'Pale-blue': 'hsl(225, 100%, 94%)',
+        'Bright-blue': 'hsl(245, 75%, 52%)',
+        // ### Neutral
+        'Very-pale-blue': 'hsl(225, 100%, 98%)',
+        'Desaturated-blue': 'hsl(224, 23%, 55%)',
+        'Dark-blue': 'hsl(223, 47%, 23%)',
+      },
+      fontFamily: {
+        redhot: ['Red Hat Display', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
